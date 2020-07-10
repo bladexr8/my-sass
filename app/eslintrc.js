d3538cc8274b122b5a8ae3632b4c5e5214912d2f
Stephen Moss <bladexr8@gmail.com>
2020-07-10 21:42:50 +1000

    Initial Config Files

---
+++ app/eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+    env: {
+        "es6": true,
+        "node": true
+    },
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                trailingComma: 'all',
+                arrowParens: 'always',
+                printWidth: 100,
+                semi: true
+            },
+        ],
+        "@typescript-eslint/camelcase": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "react/no-unescaped-entities": "off",
+        "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    },
+    plugins: [
+        "prettier"
+    ]
+}
